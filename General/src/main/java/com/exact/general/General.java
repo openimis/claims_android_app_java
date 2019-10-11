3095924a33d28631f62299dea7c5d82594e953f4
Hiren Soni <hirensoni913@outlook.com>
2019-10-11 15:06:33 +0300

    v1.3.1.1

--- General/src/main/java/com/exact/general/General.java
+++ General/src/main/java/com/exact/general/General.java
@@ -16,7 +16,7 @@ import android.os.Environment;
 import android.util.DisplayMetrics;
 
 public class General {
-	private String _Domain = "http://imis-mv.swisstph-mis.ch/";
+	private String _Domain = "http://chf-imis.or.tz/";
 
 	public String getDomain(){
 		return _Domain;
