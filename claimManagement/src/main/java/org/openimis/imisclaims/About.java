74de0eacfd4a66f4b836775c2a6d4e4238a4dbfd
vaneck237 <vaneckbilongo@gmail.com>
2025-01-29 16:47:00 +0100

    print version in about

--- claimManagement/src/main/java/org/openimis/imisclaims/About.java
+++ claimManagement/src/main/java/org/openimis/imisclaims/About.java
@@ -1,12 +1,17 @@
 package org.openimis.imisclaims;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class About extends ImisActivity {
+    TextView appVersion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        appVersion = findViewById(R.id.tvAppVersion);
+        appVersion.setText(BuildConfig.VERSION_NAME);
 
         if (actionBar != null) {
             actionBar.setTitle("");
