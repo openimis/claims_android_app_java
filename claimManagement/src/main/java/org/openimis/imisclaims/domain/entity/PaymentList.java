8090e43eba183967b8fea1e5f9c0c29831460bcf
vaneck237 <vaneckbilongo@gmail.com>
2025-07-10 12:39:35 +0100

    fix services & items filter from user healthfacility

--- claimManagement/src/main/java/org/openimis/imisclaims/domain/entity/PaymentList.java
+++ claimManagement/src/main/java/org/openimis/imisclaims/domain/entity/PaymentList.java
@@ -4,6 +4,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
@@ -15,21 +16,31 @@ public class PaymentList implements Parcelable {
     private final List<Service> services;
     @NonNull
     private final List<Medication> medications;
+    @Nullable
+    private final String servicesPricelistUuid;
+    @Nullable
+    private final String itemsPricelistUuid;
 
     public PaymentList(
             @NonNull String healthFacilityCode,
             @NonNull List<Service> services,
-            @NonNull List<Medication> medications
+            @NonNull List<Medication> medications,
+            @Nullable String servicesPricelistUuid,
+            @Nullable String itemsPricelistUuid
     ) {
         this.healthFacilityCode = healthFacilityCode;
         this.services = services;
         this.medications = medications;
+        this.servicesPricelistUuid = servicesPricelistUuid;
+        this.itemsPricelistUuid = itemsPricelistUuid;
     }
 
     protected PaymentList(Parcel in) {
         healthFacilityCode = in.readString();
         services = in.createTypedArrayList(Service.CREATOR);
         medications = in.createTypedArrayList(Medication.CREATOR);
+        servicesPricelistUuid = in.readString();
+        itemsPricelistUuid = in.readString();
     }
 
     @Override
@@ -37,6 +48,8 @@ public class PaymentList implements Parcelable {
         dest.writeString(healthFacilityCode);
         dest.writeTypedList(services);
         dest.writeTypedList(medications);
+        dest.writeString(servicesPricelistUuid);
+        dest.writeString(itemsPricelistUuid);
     }
 
     @Override
@@ -58,6 +71,12 @@ public class PaymentList implements Parcelable {
     public List<Medication> getMedications() {
         return medications;
     }
+
+    @Nullable
+    public String getServicesPricelistUuid(){ return servicesPricelistUuid; }
+
+    @Nullable
+    public String getItemsPricelistUuid(){ return itemsPricelistUuid; }
 
     public static final Creator<PaymentList> CREATOR = new Creator<>() {
         @Override
--- claimManagement/src/main/java/org/openimis/imisclaims/usecase/FetchPaymentList.java
+++ claimManagement/src/main/java/org/openimis/imisclaims/usecase/FetchPaymentList.java
@@ -7,10 +7,8 @@ import org.openimis.imisclaims.GetPaymentListQuery;
 import org.openimis.imisclaims.domain.entity.Medication;
 import org.openimis.imisclaims.domain.entity.PaymentList;
 import org.openimis.imisclaims.domain.entity.Service;
-import org.openimis.imisclaims.domain.entity.SubServiceItem;
 import org.openimis.imisclaims.network.request.GetPaymentListGraphQLRequest;
 import org.openimis.imisclaims.network.util.Mapper;
-import org.openimis.imisclaims.util.IdUtils;
 
 import java.util.Collections;
 import java.util.Objects;
@@ -37,60 +35,34 @@ public class FetchPaymentList {
         GetPaymentListQuery.HealthFacility healthFacility = Objects.requireNonNull(node.healthFacility());
         GetPaymentListQuery.ServicesPricelist services = healthFacility.servicesPricelist();
         GetPaymentListQuery.ItemsPricelist medications = healthFacility.itemsPricelist();
-        Mapper<GetPaymentListQuery.ServiceserviceSet, SubServiceItem> subServiceMapper = new Mapper<>(this::toSubService);
-        Mapper<GetPaymentListQuery.ServicesLinked, SubServiceItem> subItemMapper = new Mapper<>(this::toSubItem);
         return new PaymentList(
                 /* healthFacilityCode = */ healthFacility.code(),
-                /* services = */ services != null ? Mapper.map(services.details().edges(), dto -> toService(dto,subServiceMapper, subItemMapper)) : Collections.emptyList(),
-                /* medications = */ medications != null ? Mapper.map(medications.details().edges(), this::toMedication) : Collections.emptyList()
+                /* services = */ services != null ? Mapper.map(services.details().edges(), dto -> toService(dto)) : Collections.emptyList(),
+                /* medications = */ medications != null ? Mapper.map(medications.details().edges(), this::toMedication) : Collections.emptyList(),
+                /* servicePriceListUuid = */ healthFacility.servicesPricelist() != null ? healthFacility.servicesPricelist().uuid(): "",
+                /* servicePriceListUuid = */ healthFacility.itemsPricelist() != null ? healthFacility.itemsPricelist().uuid() : ""
         );
     }
 
     @NonNull
-    private Service toService(
-            @NonNull GetPaymentListQuery.Edge1 edge,
-            @NonNull Mapper<GetPaymentListQuery.ServiceserviceSet, SubServiceItem> subServiceMapper,
-            @NonNull Mapper<GetPaymentListQuery.ServicesLinked, SubServiceItem> subItemMapper
-    ) {
+    private Service toService(@NonNull GetPaymentListQuery.Edge1 edge) {
         GetPaymentListQuery.Service service = Objects.requireNonNull(edge.node()).service();
         return new Service(
-                /* id = */ String.valueOf(IdUtils.getIdFromGraphQLString(service.id())),
                 /* code = */ service.code(),
                 /* name = */ service.name(),
                 /* price = */ service.price(),
-                /* currency = */ "$",
-                /* packageType = */ service.packagetype(),
-                /* manualPrice = */ service.manualPrice() ? 1 : 0,
-                /* subServices = */ subServiceMapper.map(service.serviceserviceSet()),
-                /* subItems = */ subItemMapper.map(service.servicesLinked())
+                /* currency = */ "$"
         );
     }
 
     @NonNull
     private Medication toMedication(@NonNull GetPaymentListQuery.Edge2 edge) {
-        GetPaymentListQuery.Item1 item = Objects.requireNonNull(edge.node()).item();
+        GetPaymentListQuery.Item item = Objects.requireNonNull(edge.node()).item();
         return new Medication(
-                /* id = */ String.valueOf(IdUtils.getIdFromGraphQLString(item.id())),
                 /* code = */ item.code(),
                 /* name = */ item.name(),
                 /* price = */ item.price(),
                 /* currency = */ "$"
-        );
-    }
-
-    private SubServiceItem toSubService(@NonNull GetPaymentListQuery.ServiceserviceSet service) {
-        return new SubServiceItem(
-                /* id = */ String.valueOf(IdUtils.getIdFromGraphQLString(service.service().id())),
-                /* quantity = */ service.qtyProvided(),
-                /* price = */ service.priceAsked()
-        );
-    }
-
-    private SubServiceItem toSubItem(@NonNull GetPaymentListQuery.ServicesLinked item) {
-        return new SubServiceItem(
-                /* id = */ String.valueOf(IdUtils.getIdFromGraphQLString(item.item().id())),
-                /* quantity = */ item.qtyProvided(),
-                /* price = */ item.priceAsked()
         );
     }
 }
